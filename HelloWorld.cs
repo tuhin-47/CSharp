ec3cf2a36d46c41481f04ab7ad770db8c64b28dc
tuhin-47 <tuhin.mc@gmail.com>
2023-05-10 15:49:54 +0600

    First Commit

---
+++ HelloWorld.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace CSharp{
+    internal class HelloWorld{
+        public static void Main(string[] args){
+            Console.WriteLine("Hello World\n Date: 10 May 2023");
+        }
+    }
+}
