2498577a7ec65a2c9e4a8f957495611ad2d9398d
tuhin-47 <tuhin.mc@gmail.com>
2023-05-10 17:25:11 +0600

    NewClass Added

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -6,4 +6,11 @@ namespace CSharp{
             Console.WriteLine("Hello World\n Date: 10 May 2023");
         }
     }
+
+    public class NewClass{
+        public static void showMsg(){
+            Console.WriteLine("msg from NewClass.showMsg()");
+        }
+    }
 }
+
