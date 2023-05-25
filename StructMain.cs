c88e98cb54edd8a337c6e90eddf876c6a8457499
tuhin-47 <tuhin.mc@gmail.com>
2023-05-25 13:08:16 +0600

    Main as a member of struct

---
+++ StructMain.cs
@@ -0,0 +1,9 @@
+// Main method can be member of struct.
+
+using System;
+
+struct StructMainDemo{
+	static void Main(string[] args){
+		Console.WriteLine(" static void Main()  | as a member of struct");
+	}
+}
